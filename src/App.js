cb0ed979562adadbf141d2eff83dc195ea79bfcd
raxit1902 <raxit1902@gmail.com>
2023-11-28 13:00:27 +0530

    git cache cleared

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 class="text-3xl font-bold underline text-red-500">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        Hello world!
+      </h1>
     </div>
   );
 }
