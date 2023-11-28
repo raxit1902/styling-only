919895ef5c34b12210659986c9a16dbee05ac4fc
raxit1902 <raxit1902@gmail.com>
2023-11-28 11:21:26 +0530

    Modified Index.css

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode>6
     <App />
   </React.StrictMode>
 );
